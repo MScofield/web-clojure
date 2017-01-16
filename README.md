a46f2fa4f3085f2c7172ffd5822874f641cf96eb
Matthew Scofield <matthewgscofield@outlook.com>
2017-01-16 15:12:02 -0600

    deterministic functions work, turning it in

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # web-clojure
 
-A Clojure app to ... well, that part is up to you.
+web-clojure is a web "app that does something" per the assignment instructions. 
 
-## Usage
+Since the criteria was so open-ended I decided to make a "decoder ring" of sorts,
+in addition to saving to and retreiving from an .edn file the app uses the code entered
+to define the number of permutations applied over a string of text. The changes happen in
+a determined order by index so each outcome is predictable.
 
-FIXME
-web-clojure.core=> (map (fn [authenticode] (get authenticode :authenticode)) @authenticodes)
-("12345")
+Next time you have an important email to send use Authenticode to create a security signature! 
+
+Simply:
+1. enter your own six-digit code on our web app, 
+2. add the signature produced by the app to your email, 
+3. then text the code you used to your recipient!  
+
+The recipient will be able to:
+1. enter the code in the Authenticode web app
+2. and see the same signature! 
+3. Verified! 
--- src/web_clojure/core.clj
+++ src/web_clojure/core.clj
@@ -9,41 +9,15 @@
 (defonce server (atom nil))
 (defonce authenticodes (atom []))
 
-(c/defroutes app
-  (c/GET "/" []
-    (h/html [:html]
-            [:body 
-             [:h2 "Authenticate Each Message Received with AuthentiCode!!!"]
-             [:form {:action "/add-authenticode" :method "post"}
-              [:input {:type "text" :placeholder "Enter AuthentiCode" :name "authenticode"}"\u00A0"
-               [:input {:type "text" :placeholder "Log Sender" :name "sender"}]"\u00A0"
-               [:button {:type "submit"} "Check Code"]] 
-              [:hr]
-              [:table (:style "border: 0; width: 90%")
-               [:tr [:th "Code Log""\u00A0 \u00A0 \u00A0"][:th "Sender Log""\u00A0 \u00A0 \u00A0"][:th "Authenticity Confirmation"]]
-               [:td
-                     (map (fn [authenticode] (get authenticode :authenticode)
-                            (get authenticode :sender)
-                            [:tr authenticode [:td (get authenticode :authenticode)]
-                             [:td (get authenticode :sender)]])
-                       @authenticodes)]]]]))
-  
-  (c/POST "/add-authenticode" request
-    (let [params (get request :params)
-          authenticode (get params "authenticode")
-          sender (get params "sender")
-          authenticodeRecord (zipmap [:authenticode :sender] [authenticode sender])]
-      (swap! authenticodes conj authenticodeRecord)
-      (spit "authenticodes.edn" (pr-str @authenticodes))
-      (r/redirect "/"))))
-
 (def factorial (reductions * 1 (drop 1 (range))))
+(def colours ["-red-" "-blue-" "-green-" "-yellow-" "-cyan-" "-magenta-" "-black-" "-white-"])
 
 (defn factoradic [n] {:pre [(>= n 0)]}
    (loop [a (list 0) n n p 2]
       (if (zero? n) a (recur (conj a (mod n p)) (quot n p) (inc p)))))
 
-(defn nth-permutation [s n] {:pre [(< n (nth factorial (count s)))]}
+(defn nth-permutation [s n] (println s)(println n)
+  {:pre [(< n (nth factorial (count s)))]}
   (let [d (factoradic n)
         choices (concat (repeat (- (count s) (count d)) 0) d)]
     ((reduce 
@@ -52,6 +26,41 @@
             (assoc m :rem (concat left right) 
                      :acc (conj (m :acc) item))))
       {:rem s :acc []} choices) :acc)))
+
+(c/defroutes app
+  (c/GET "/" []
+    (h/html [:html]
+            [:body 
+             [:h2 "Authenticate Your Important Emails with AuthentiCode!!!"]
+             [:form {:action "/add-authenticode" :method "post"}
+              [:input {:type "text" :placeholder "Enter AuthentiCode" :name "authenticode"}"\u00A0"
+               [:input {:type "text" :placeholder "Log Sender" :name "sender"}]"\u00A0"
+               [:input {type "text" :placeholder "Log Recipient" :name "recipient"}]"\u00A0" 
+               [:button {:type "submit"} "Check Code"]] 
+              [:hr]
+              [:table (:style "border: 0; width: 90%")
+               [:tr [:th "Code Log""\u00A0 \u00A0 \u00A0"][:th "Sender Log""\u00A0 \u00A0 \u00A0"][:th "Receiver Log""\u00A0\u00A0\u00A0"][:th "Authenticity Confirmation"]]
+               [:td
+                     (map (fn [authenticode] (get authenticode :authenticode)
+                            (get authenticode :sender)
+                            [:tr authenticode 
+                             [:td (get authenticode :authenticode)]
+                             [:td (get authenticode :sender)]
+                             [:td (get authenticode :recipient)]
+                             [:td (get authenticode :keyOrder)]])
+                       @authenticodes)]]]]))
+  
+  (c/POST "/add-authenticode" request
+    (let [params (get request :params)
+          authenticode (Integer/parseInt (get params "authenticode"))
+          sender (get params "sender")
+          recipient (get params "recipient")
+          keyOrder (remove nil? (nth-permutation colours authenticode))
+          authenticodeRecord (zipmap [:authenticode :sender :recipient :keyOrder] [authenticode sender recipient keyOrder])]
+      (swap! authenticodes conj authenticodeRecord)
+      (spit "authenticodes.edn" (pr-str @authenticodes))
+      (println (remove nil? (nth-permutation colours authenticode)))
+      (r/redirect "/"))))
 
 (defn -main []
   (try
